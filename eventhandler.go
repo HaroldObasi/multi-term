d8d3094ebbd7123b024661d6457a31062ae55b2d
Harold Obasi <haroldobasi2k16@gmail.com>
2024-08-30 21:50:15 -0100

    moved event handling to separate file

--- eventhandler.go
+++ eventhandler.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func HandleEvents(screen *Screen){
+	tScreen := screen.tScreen
+	for {
+		switch ev := tScreen.PollEvent().(type) {
+		case *tcell.EventResize:
+			tScreen.Sync()
+		case *tcell.EventKey:
+			switch ev.Key() {
+
+			case tcell.KeyEscape:
+				tScreen.Fini()
+				os.Exit(0)
+
+				// case tcell.KeyRune:
+				// 	ch := ev.Rune()
+				// 	w.Write(ch, s)
+
+				case tcell.KeyBackspace, tcell.KeyBackspace2:
+					// screen.Delete(s)
+					screen.WriteDebug( "Backspace")
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/gdamore/tcell/v2"
 )
 
 // This program just prints "Hello, World!".  Press ESC to exit.
@@ -15,27 +13,5 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	tScreen := screen.tScreen
-
-	for {
-		switch ev := tScreen.PollEvent().(type) {
-		case *tcell.EventResize:
-			tScreen.Sync()
-		case *tcell.EventKey:
-			switch ev.Key() {
-
-			case tcell.KeyEscape:
-				tScreen.Fini()
-				os.Exit(0)
-
-				// case tcell.KeyRune:
-				// 	ch := ev.Rune()
-				// 	w.Write(ch, s)
-
-				case tcell.KeyBackspace, tcell.KeyBackspace2:
-					// screen.Delete(s)
-					screen.WriteDebug( "Backspace")
-			}
-		}
-	}
+	HandleEvents(screen)
 }
