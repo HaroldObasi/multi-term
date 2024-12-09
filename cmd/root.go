a85aaaa9ef1265f0ef03c53a04c927523ec504d4
Harold Obasi <haroldobasi2k16@gmail.com>
2024-12-09 15:31:02 +0100

    cobra setup

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mt",
+	Short: "multi term is a cool little text editor",
+	Long: `Generate and deploy static site portfolio
+	
+	Example: mt -f testing.txt`,
+	
+	Run:     RunCommand,
+	PreRunE: PreRunChecks,
+}
+
+func init() {
+	rootCmd.Flags().StringP("fileName", "f", "", "Filename")
+}
+
+func RunCommand(cmd *cobra.Command, args []string) {
+	fileName, _ := cmd.Flags().GetString("fileName")
+	fmt.Println("Editing file: ", fileName)
+}
+
+func PreRunChecks(cmd *cobra.Command, args []string) error {
+	fileName, _ := cmd.Flags().GetString("fileName")
+
+	if fileName == "" {
+		return fmt.Errorf("filename is required")
+	}
+
+	return nil
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/HaroldObasi/multi-term/cmd"
+)
 
 func main()  {
-	fmt.Println("hello world")
+	cmd.Execute()
 }
