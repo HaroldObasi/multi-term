d82d99bfab6975a93fd9047b086106e73f5424cf
Harold Obasi <haroldobasi2k16@gmail.com>
2024-12-09 19:42:11 +0100

    function to create test window

--- window/window.go
+++ window/window.go
@@ -1,7 +1,6 @@
 package window
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/HaroldObasi/multi-term/buffer"
@@ -9,41 +8,62 @@ import (
 )
 
 type Window struct {
-	screen tcell.Screen
+	Screen tcell.Screen
 	screenStyle tcell.Style
-	tab *buffer.TabBuffer
+	Tab *buffer.TabBuffer
 }
 
 func NewWindow() (*Window, error) {
 	win := &Window{}
-	screen, err := tcell.NewScreen()
+	Screen, err := tcell.NewScreen()
 
 	if err != nil {
 		return nil, err
 	}
 	
-	if err := screen.Init(); err != nil {
+	if err := Screen.Init(); err != nil {
 		return nil, err
 	}
 
-	win.screen = screen // Assign screen to window
+	win.Screen = Screen // Assign Screen to window
 
 	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
 
 	win.screenStyle = defStyle // Assign default style to window
-	win.screen.SetStyle(win.screenStyle)
+	win.Screen.SetStyle(win.screenStyle)
 
-	tb := buffer.NewTabBuffer(win.screen)
-	win.tab = tb // Assign tab buffer to window
+	tb := buffer.NewTabBuffer(win.Screen)
+	win.Tab = tb // Assign tab buffer to window
 
+
+	return win, nil
+}
+
+func NewTestWindow() (*Window, error) {
+	win := &Window{}
+	Screen := tcell.NewSimulationScreen("UTF-8")
+	
+	if err := Screen.Init(); err != nil {
+		return nil, err
+	}
+
+	win.Screen = Screen // Assign Screen to window
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+
+	win.screenStyle = defStyle // Assign default style to window
+	win.Screen.SetStyle(win.screenStyle)
+
+	tb := buffer.NewTabBuffer(win.Screen)
+	win.Tab = tb // Assign tab buffer to window
 
 	return win, nil
 }
 
 func (win *Window) Start() error {
 	for {
-		// Update screen
-		win.screen.Show()
+		// Update Screen
+		win.Screen.Show()
 	
 		// Poll and Handle events
 		win.HandleEvents()
@@ -52,24 +72,25 @@ func (win *Window) Start() error {
 }
 
 func (win *Window) HandleEvents() {
-	ev := win.screen.PollEvent()
+	ev := win.Screen.PollEvent()
 
 	// Process event
 	switch ev := ev.(type) {
 	case *tcell.EventResize:
-		win.screen.Sync()
+		win.Screen.Sync()
 	case *tcell.EventKey:
 		if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
 			win.Quit()
 		}
 		switch ev.Key(){
 		case tcell.KeyRune:
-			fmt.Print("rune")
+			ch := ev.Rune()
+			win.Tab.InsertRune(ch)
 		}
 	}
 }
 
 func (win *Window) Quit() {
-	win.screen.Fini()
+	win.Screen.Fini()
 	os.Exit(0)
 }
