19f9e5fb703896649e610ed4e989180404d4bb22
Harold Obasi <haroldobasi2k16@gmail.com>
2024-12-09 19:41:58 +0100

    cursor control methods

--- cursor/cursor.go
+++ cursor/cursor.go
@@ -16,3 +16,49 @@ func NewCursor(screen tcell.Screen) *Cursor {
 		Y: Y,
 	}
 }
+
+func (cursor Cursor) GetPos() (int, int) {
+	return cursor.X, cursor.Y
+}
+
+func (cursor *Cursor) GoTo(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	}
+
+	if x > cursor.X {
+		for i := 0; i < x-cursor.X; i++ {
+			cursor.goRight()
+		}
+	} else if x < cursor.X {
+		for i := 0; i < cursor.X-x; i++ {
+			cursor.goLeft()
+		}
+	} else if y > cursor.Y {
+		for i := 0; i < y-cursor.Y; i++ {
+			cursor.goDown()
+		}
+	} else if y < cursor.Y {
+		for i := 0; i < cursor.Y-y; i++ {
+			cursor.goUp()
+		}
+	}
+
+	cursor.screen.ShowCursor(cursor.X, cursor.Y)
+}
+
+func (cursor *Cursor) goLeft() {
+	cursor.X--
+}
+
+func (cursor *Cursor) goRight() {
+	cursor.X++
+}
+
+func (cursor *Cursor) goUp() {
+	cursor.Y--
+}
+
+func (cursor *Cursor) goDown() {
+	cursor.Y++
+}
