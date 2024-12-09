c456a055e471b2bed2c5770b1e5f665e97f4394f
Harold Obasi <haroldobasi2k16@gmail.com>
2024-12-09 19:55:42 +0100

    refactor tests in window_test.go to use insertString instead of insertRune

--- window/window_test.go
+++ window/window_test.go
@@ -8,12 +8,12 @@ import (
 	"github.com/HaroldObasi/multi-term/window"
 )
 
-func TestNewWindow(t *testing.T) {
-	_, err := window.NewTestWindow()
-	if err != nil {
-		t.Fatalf("Error creating new window: %v", err)
-	}
-}
+// func TestNewWindow(t *testing.T) {
+// 	_, err := window.NewTestWindow()
+// 	if err != nil {
+// 		t.Fatalf("Error creating new window: %v", err)
+// 	}
+// }
 
 func TestInsert(t *testing.T){
 	win, err := window.NewTestWindow()
@@ -29,15 +29,22 @@ func TestInsert(t *testing.T){
         initialText   string
         gapSize      int
         insertPos    int  // cursor position where we'll insert
-        insertRune   rune
+        insertString   string
         wantText     string
         wantCursorX  int
     }{
         {
             name:        "insert 'x' in empty buffer",
             insertPos:   0,
-            insertRune:  'x',
+            insertString:  "x",
             wantText:    "x",
+            wantCursorX: 1,
+        },
+        {
+            name:        "insert '1234567890' in empty buffer",
+            insertPos:   0,
+            insertString:  "1234567890",
+            wantText:    "1234567890",
             wantCursorX: 1,
         },
     }
@@ -45,7 +52,11 @@ func TestInsert(t *testing.T){
 	for _, tt := range tests {	
 		t.Run(tt.name, func(t *testing.T) {
 			tab := buffer.NewTabBuffer(win.Screen)
-			tab.InsertRune(tt.insertRune)
+
+			for _, ch := range tt.insertString{
+				tab.InsertRune(ch)
+			}
+			
 			line := tab.Lines[0]
 			fmt.Println("Line: ", line)
 		})
