85ce9ed89fd08b9b9e057977cb9f1e19d03b3811
Harold Obasi <haroldobasi2k16@gmail.com>
2025-05-13 12:32:18 +0100

    move line buffer gap on cursor move

--- buffer/tab.go
+++ buffer/tab.go
@@ -36,17 +36,29 @@ func (tb *TabBuffer) InsertRune(r rune) {
 	line.InsertRune(r)
 }
 
+func (tb *TabBuffer) InsertString(s string) {
+	// iterate through the string and insert each rune
+	for _, r := range s {
+		tb.InsertRune(r)
+	}
+
+}
+
 func (tb *TabBuffer) GoTo(i int) {
 
 }
 
 func (tb *TabBuffer) GoLeft() {
 	X, Y := tb.Cursor.GetPos()
+	if X < 0 || Y < 0 {
+		return
+	}
 	tb.Cursor.GoTo(
 		X-1,
 		Y,
 	)
-
+	currentLine := tb.Lines[Y]
+	currentLine.GoLeft()
 }
 
 func (tb *TabBuffer) GoRight() {
@@ -55,6 +67,8 @@ func (tb *TabBuffer) GoRight() {
 		X+1,
 		Y,
 	)
+	currentLine := tb.Lines[Y]
+	currentLine.GoRight()
 }
 
 func (tb *TabBuffer) CursorUp() {
